5b0c5fa742e1dc6ba51af3590395eee10c64f43d
guoshusong <g15176193717@gmail.com>
2019-07-18 21:20:03 +0800

    线性表链式存储结构

---
+++ C C++/线性表链式存储结构.c
@@ -0,0 +1,74 @@
+
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+typedef int Status;
+typedef int ElemType;
+typedef struct Node
+{
+	ElemType data;
+	struct Node *next;
+}node;
+
+typedef struct Node *LinkList;
+
+Status GetElem(LinkList L, int i, ElemType *e) {
+	int j;
+	LinkList p;
+	p = L->next;
+	j = 1;
+	while (p && j < i)//p不为空，j还没有等于i时,循环继续
+	{
+		p = p->next;
+		++j;
+	}
+	if (!p || j > i )
+	{
+		return ERROR;
+	}
+	*e = p->data;
+	return OK;
+}
+
+Status ListInsert(LinkList* L, int i, ElemType e) {
+	int j;
+	LinkList p, s;
+	p = *L;
+	j = 1;
+	while (p && j < i ) {
+		p = p->next;
+		++j;
+	}
+	if (!p || j > i)
+	{
+		return ERROR;
+	}
+	s = (LinkList)malloce(sizeof(Node) );//生成新结点(C标准函数)
+
+	s->data = e;
+	s->next = p->next;
+	p->next = s;
+	return OK;
+}
+
+Status ListInsert(LinkList* L, int i, ElemType* e) {
+	int j;
+	LinkList p, q;
+	p = *L;
+	j = 1;
+	while (p && j < i) {
+		p = p->next;
+		++j;
+	}
+	if (!p || j > i)
+	{
+		return ERROR;
+	}
+	q = p->next;
+	p->next = q->next;
+	*e = q->data;
+	free(q);
+	return OK;
+}
